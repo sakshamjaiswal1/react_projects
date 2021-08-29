9e5fef39219fcee671f4dbb00ef5cbd0b42c2eeb
Saksham Jaiswal <sjais973@gmail.com>
2021-08-29 17:37:42 +0530

    music

---
+++ music_player/react_music/src/Smusic.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function smusic() {
+  return (
+    <div>
+      <h1 style={{ color: "red", left: "100px" }}>S-Music</h1>
+    </div>
+  );
+}
+
+export default smusic;
